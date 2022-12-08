2b6b62ed93a1f857b25f65eb662efa903cf65288
thepolm3 <thepolm3@gmail.com>
2022-12-08 22:08:35 +0000

    add some documentation to my awful function

--- src/bin/day8.rs
+++ src/bin/day8.rs
@@ -1,35 +1,45 @@
-use std::{fs, io::BufRead};
+use std::fs;
 
+/// Traverses the forest exactly once
+/// uses two pointers that converge for the left and right trees
+/// uses a [u8; N] for the tallest tree visible in column i from the top
+/// uses the lower 10 bits of a [u16; N] to store the trees visible from the bottom in columnn i
 fn part1<const N: usize>(rows: &[&[u8]]) -> u32 {
     let mut bottom = [0; N]; //bitmask for trees visible from the bottom
     let mut top = [0u8; N]; //max values visible from above
-    let mut front_ptr: usize;
-    let mut back_ptr: usize;
-    let mut front: u8;
-    let mut back: u8;
-    let mut current: u8;
+    let [mut front_ptr, mut back_ptr]: [usize; 2];
+    let [mut front, mut current, mut back]: [u8; 3];
     let mut include: bool;
     let mut result = 0;
 
     for row in rows {
         (front_ptr, back_ptr, front, back) = (0, N - 1, b'0' - 1, b'0' - 1);
-        while front_ptr <= back_ptr {
+
+        // move the pointers together till they kiss
+        for _ in 0..N {
             include = false;
             if front <= back {
                 current = row[front_ptr];
+
+                //front check
                 if current > front {
                     include = true;
                     front = current;
                 }
+
+                //top check
                 if current > top[front_ptr] {
                     include = true;
                     top[front_ptr] = current;
                 }
-                bottom[front_ptr] &= !(1u16) << (current - b'0'); //if including it should block and not be included
-                bottom[front_ptr] |= (!include as u16) << (current - b'0');
+
+                //bottom check
+                bottom[front_ptr] &= !(1u16) << (current - b'0'); //zero out all trees at or below current height
+                bottom[front_ptr] |= (!include as u16) << (current - b'0'); //if !include, add back in tree at current height
 
                 front_ptr += 1;
             } else {
+                // same logic but for back of list
                 current = row[back_ptr];
                 if current > back {
                     include = true;
@@ -44,6 +54,7 @@ fn part1<const N: usize>(rows: &[&[u8]]) -> u32 {
                 bottom[back_ptr] |= (!include as u16) << (current - b'0');
                 back_ptr -= 1;
             }
+
             if include {
                 result += 1;
             }
@@ -54,6 +65,7 @@ fn part1<const N: usize>(rows: &[&[u8]]) -> u32 {
 
 fn scenic_score(rows: &[&[u8]], (start_x, start_y): (isize, isize)) -> isize {
     let height = rows[start_y as usize][start_x as usize];
+
     [(1, 0), (-1, 0), (0, 1), (0, -1)]
         .into_iter()
         .map(|(dx, dy)| {
