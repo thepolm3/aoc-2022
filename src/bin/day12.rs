b7ec54b5790cb679095afde0e95985a7ec1c433a
thepolm3 <thepolm3@gmail.com>
2022-12-12 16:31:05 +0000

    big speedup all around

--- src/bin/day12.rs
+++ src/bin/day12.rs
@@ -1,5 +1,4 @@
 use anyhow::{Context, Result};
-use itertools::Itertools;
 
 #[derive(Debug, Eq, PartialEq)]
 struct GridSquare {
@@ -43,42 +42,51 @@ fn main() -> Result<()> {
 
     let mut to_search: Vec<usize>;
     let mut next_search = vec![end];
-    let mut part2 = Some(0);
+    let mut part2 = None;
 
+    let now = std::time::Instant::now();
     grid[end].distance = Some(0);
-    'outer: for i in 0_usize.. {
-        to_search = next_search.into_iter().unique().collect();
-        next_search = Vec::new();
+    'outer: for i in 1_usize.. {
+        (next_search, to_search) = (Vec::new(), next_search);
         if to_search.is_empty() {
             break 'outer;
         }
         while let Some(cur) = to_search.pop() {
-            grid[cur].distance = Some(i);
             if cur == start {
                 break 'outer;
             }
-            if grid[cur].height == 0 {
+            if grid[cur].height == 0 && part2.is_none() {
                 part2 = grid[cur].distance;
             }
-            next_search.extend(
-                [cur.wrapping_sub(1), cur + 1, cur.wrapping_sub(w), cur + w]
-                    .into_iter()
-                    .filter_map(|nbr| match grid.get(nbr).and_then(|x| x.distance) {
-                        Some(_) => None,
-                        None => grid.get(cur).and_then(|to| {
-                            grid.get(nbr).and_then(|from| {
-                                (to.height.saturating_sub(from.height) <= 1).then_some(nbr)
-                            })
-                        }),
-                    }),
-            );
+            for nbr in [cur.wrapping_sub(1), cur + 1, cur.wrapping_sub(w), cur + w] {
+                if nbr >= grid.len() {
+                    continue;
+                }
+                if let (Some(from), Some(to)) = match nbr < cur {
+                    true => {
+                        let split_grid = grid.split_at_mut(cur);
+                        (split_grid.0.get_mut(nbr), split_grid.1.get_mut(0))
+                    }
+                    false => {
+                        let split_grid = grid.split_at_mut(nbr);
+                        (split_grid.1.get_mut(0), split_grid.0.get_mut(cur))
+                    }
+                } {
+                    if from.distance.is_some() {
+                        continue;
+                    };
+                    if to.height.saturating_sub(from.height) > 1 {
+                        continue;
+                    };
+                    from.distance = Some(i);
+                    next_search.push(nbr);
+                };
+            }
         }
     }
-    println!(
-        "12.1 {:?}",
-        grid[start].distance.context("no solution for part 1")?
-    );
-    println!("12.2 {:?}", part2.context("no solution for part 2")?);
+    println!("12.1 {:?}", grid[start].distance.context("no solution p1")?);
+    println!("12.2 {:?}", part2.context("no solution p2")?);
+    println!("{:?}", now.elapsed());
 
     Ok(())
 }
